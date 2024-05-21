5b227f73bcb9eb23a4d7ce32500786d68ccca6b5
Clifford Silindana <cliffords2105@gmail.com>
2024-05-21 21:34:10 +0200

    MySQL script to create clients table with client number and email columns.

---
+++ psg_clients_db.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE psg_client_db;
+
+USE psg_client_db;
+
+CREATE TABLE clients (
+    client_id INT AUTO_INCREMENT PRIMARY KEY,
+    client_number VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL
+);
+
+-- SHOW TABLES;
+
+-- DESC clients;
+
+INSERT INTO clients (client_number, email) VALUES ('12345', 'client1@example.com');
+INSERT INTO clients (client_number, email) VALUES ('67890', 'client2@example.com');
+INSERT INTO clients (client_number, email) VALUES ('54321', 'client3@example.com');
+INSERT INTO clients (client_number, email) VALUES ('09876', 'client4@example.com');
+
+-- SELECT * FROM clients;
+
+
