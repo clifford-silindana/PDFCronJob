5597680d4e840c02dde4789d8d3c881bb92e7a04
Clifford Silindana <cliffords2105@gmail.com>
2024-05-22 05:13:34 +0200

    Script successfully sends emails.

--- script.py
+++ script.py
@@ -9,17 +9,17 @@ from email.mime.text import MIMEText
 
 # Define the path to the PDF folder as one directory above the current script location
 current_dir = os.path.dirname(os.path.abspath(__file__))
-PDF_FOLDER = os.path.join(current_dir, '..', 'pdfs')
+PDF_FOLDER = os.path.join(current_dir, '..', 'PDFs')
 
 DB_HOST = "localhost"
 DB_PORT = 3306
 DB_USER = "root"
 DB_PASSWORD = "silindana05100%"
 DB_NAME = "psg_client_db"
-EMAIL_HOST = ""
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USER = "clifford.silindana@gmail.com"
-EMAIL_PASSWORD = ""
+EMAIL_PASSWORD = "zgkj asxa hlgp gtmf"
 
 def get_client_email(client_number):
     conn = mysql.connector.connect(
@@ -59,6 +59,26 @@ def main():
     for filename in os.listdir(PDF_FOLDER):
         if filename.endswith(".pdf"):
             client_number = filename.split(".")[0] # Assuming the client number is the part before the first dot
+            client_email = get_client_email(client_number)
+
+            if client_email:
+                attachment_path = os.path.join(PDF_FOLDER, filename)
+                send_email(
+                    to_address = client_email,
+                    subject = "Your PDF document",
+                    body = "Good day. Please find your PDF document attached.",
+                    attachment_path = attachment_path
+                )
+
+                print(f'Sent {filename} to {client_email}')
+            else:
+                print(f'No email found for client number {client_number}')
+
+if __name__ == "__main__":
+    main()
+
+
+
 
 
 
